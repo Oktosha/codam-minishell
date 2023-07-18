3479ba7b77381c4fad49c2f41f73cd5ee7170415
Lena <110818881+elenavoronin@users.noreply.github.com>
2023-07-18 14:07:54 +0000

    dev: parse structs

--- kotishell/include/parse.h
+++ kotishell/include/parse.h
@@ -6,7 +6,7 @@
 /*   By: elenavoronin <elnvoronin@gmail.com>          +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/06/25 15:06:03 by mbp14         #+#    #+#                 */
-/*   Updated: 2023/06/28 20:04:59 by elenavoroni   ########   odam.nl         */
+/*   Updated: 2023/07/18 14:07:35 by codespace     ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,21 @@
 */
 #ifndef PARSE_H
 # define PARSE_H
+# include "lists.h"
+
+typedef struct s_ps_single_command
+{
+	char			**argv;
+	t_li_node		*inputs;
+	t_li_node		*outputs;
+	t_li_node		*appends;
+}	t_ps_single_command;
+
+typedef struct s_ps_command_list
+{
+	t_li_node	*cmds;
+	char		*heredoc;
+	int			heredoc_nr;
+}	t_ps_command_list;
 
 #endif
