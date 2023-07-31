a3b219080c2157e3a2131dbaefaf43e23e0d3777
Daria Kolodzey <kolodaria@gmail.com>
2023-07-31 21:55:47 +0200

    dev: Extend minilibft

--- kotishell/include/minilibft.h
+++ kotishell/include/minilibft.h
@@ -6,7 +6,7 @@
 /*   By: elenavoronin <elnvoronin@gmail.com>          +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/06/30 15:23:07 by evoronin      #+#    #+#                 */
-/*   Updated: 2023/07/21 15:35:40 by elenavoroni   ########   odam.nl         */
+/*   Updated: 2023/07/31 21:21:28 by dkolodze      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,35 @@ int			mini_putstr_fd(const char *s, int fd);
 void		*mini_malloc(size_t size);
 int			mini_strlen(const char *s);
 void		mini_assert(int condition, const char *str);
+
+/**
+ * @brief searches for the first occurence of ch in null-terminated string s
+ * @param s null-terminated string
+ * @param ch character to search for
+ * @return position of character in s or -1 of no such character in string
+ * @note s must be a valid string, not NULL
+ * @note ch can be '\0' in that case string length is returned
+*/
+int			mini_find(char *s, char ch);
+
+/**
+ * @brief creates (allocates) a copy of a null-terminated string s
+ * @note s can't be null or any other invalid pointer
+ * @return copied string or NULL on failed allocation
+*/
+char		*mini_strdup(const char *s);
+
+/**
+ * @brief creates (allocates) string s1s2 out of strings s1 and s2
+ * @note s1 and s2 must be valid strings, not nulls
+ * @note s1 and s2 may be empty strings (but must be null-terminated)
+ * @return concatenated string or NULL on failed allocation 
+*/
+char		*mini_concat(const char *s1, const char *s2);
+
+/**
+ * @brief calls free and returns NULL; used to save lines
+*/
+void		*mini_free(void *p);
 
 #endif
--- test/mini_tests/mini_main.c
+++ test/mini_tests/mini_main.c
@@ -6,15 +6,27 @@
 /*   By: elenavoronin <elnvoronin@gmail.com>          +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/07/21 15:33:10 by elenavoroni   #+#    #+#                 */
-/*   Updated: 2023/07/21 15:33:36 by elenavoroni   ########   odam.nl         */
+/*   Updated: 2023/07/31 21:52:37 by dkolodze      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <string.h>
 #include "minilibft.h"
 
 int main (void)
 {
-	int size = 0;
-	mini_malloc(size);
+	mini_assert(mini_find("aac", 'a') == 0, "mini_find finds first symbol");
+	mini_assert(mini_find("aac", 'c') == 2, "mini_find finds last symbol");
+	mini_assert(mini_find("aac", '\0') == 3, "mini_find of '\\0' is length");
+	mini_assert(mini_find("", 'a') == -1, "mini_find doesn't find anything in empty string");
+	mini_assert(mini_find("avdsvfdvfdvb", 'Z') == -1, "mini_find returns -1 if the symbol is absent");
+
+	char *s = mini_concat("ab", "CDEF");
+	mini_assert(strcmp(s, "abCDEF") == 0, "ab + CD = abCDEF");
+	free(s);
+
+	s = mini_strdup("abc");
+	mini_assert(strcmp(s, "abc") == 0, "duplicate of abc is abc");
+	mini_free(s);
 	return (0);
 }
