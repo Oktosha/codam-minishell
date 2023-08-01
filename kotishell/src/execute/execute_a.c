5d261cca48a9ce973ab6e862f145d329456002cf
elenavoronin <elnvoronin@gmail.com>
2023-08-01 18:11:23 +0200

    dev: lifecycle print update

--- kotishell/src/execute/execute_a.c
+++ kotishell/src/execute/execute_a.c
@@ -6,7 +6,7 @@
 /*   By: elenavoronin <elnvoronin@gmail.com>          +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/07/25 15:48:11 by elenavoroni   #+#    #+#                 */
-/*   Updated: 2023/07/25 18:47:23 by dkolodze      ########   odam.nl         */
+/*   Updated: 2023/08/01 18:11:09 by elenavoroni   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,10 @@
 
 void	ex_execute(t_ks_kotistate *state, t_li_node *cmds)
 {
+	t_ps_single_command	*s_cmd;
+
 	(void) state;
 	(void) cmds;
-	printf("I AM EXECUTING\n");
+	s_cmd = cmds->data;
+	printf("I AM EXECUTING: %s\n", (char *)s_cmd->argv->data);
 }
