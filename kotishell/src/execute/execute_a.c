8192e9a446d1e80d9b067c71eaac535c3004eda7
Daria Kolodzey <kolodaria@gmail.com>
2023-08-09 16:25:16 +0000

    dev: better close in children

--- kotishell/src/execute/execute_a.c
+++ kotishell/src/execute/execute_a.c
@@ -6,7 +6,7 @@
 /*   By: elenavoronin <elnvoronin@gmail.com>          +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/07/25 15:48:11 by elenavoroni   #+#    #+#                 */
-/*   Updated: 2023/08/04 14:27:43 by dkolodze      ########   odam.nl         */
+/*   Updated: 2023/08/09 16:17:11 by dkolodze      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 
 int	ex_execute(t_ks_kotistate *state, t_li_node *cmds)
 {
-	int					n_pipes;
-	t_ex_pipe_array		pipes;
-	int					retcode;
+	int				n_pipes;
+	t_ex_plumbing	plumbing;
+	int				retcode;
 
 	if (li_list_size(cmds) == 0)
 		return (state->status_code);
@@ -24,11 +24,11 @@ int	ex_execute(t_ks_kotistate *state, t_li_node *cmds)
 		&& l_ex_is_builtin(((t_ps_single_command *)cmds->data)->argv->data))
 		return (l_ex_builtin(state, cmds->data));
 	n_pipes = li_list_size(cmds) - 1;
-	pipes = l_ex_create_pipes(n_pipes);
-	if (pipes == NULL)
+	plumbing = l_ex_create_plumbing(n_pipes);
+	if (plumbing.fildesarr == NULL)
 		return (88);
-	l_ex_launch_all(state, cmds, pipes);
+	l_ex_launch_all(state, cmds, plumbing);
 	retcode = l_ex_wait_all(cmds);
-	l_ex_cleanup_pipes(n_pipes, &pipes);
+	l_ex_full_plumbing_cleanup(&plumbing);
 	return (retcode);
 }
