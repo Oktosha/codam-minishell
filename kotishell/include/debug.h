9e92a43ac08eb19ff945b5aa7d7d085df0e66c70
Daria Kolodzey <kolodaria@gmail.com>
2023-08-14 13:13:50 +0000

    dev: Create debug.h

---
+++ kotishell/include/debug.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   debug.h                                            :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: codespace <codespace@student.codam.nl>       +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/08/11 11:13:51 by codespace     #+#    #+#                 */
+/*   Updated: 2023/08/11 11:22:18 by codespace     ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+/** @file 
+ * Module for debug printing as well as creating and comparing custom objects
+ * The "production" versions of functions do nothing, real ones are in test
+ * Namespace prefix: "dbg".
+*/
+#ifndef DEBUG_H
+# define DEBUG_H
+
+
+
+#endif
